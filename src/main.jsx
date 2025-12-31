abc4e83938a3261b6d7d6d222b7246177807510d
shennawy <ahmedshennawy997@gmail.com>
2025-12-31 12:29:13 +0200

    fuck mobile app

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import ThemeProvider from "./Components/Contexts/ThemeProvider.jsx";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js");
-  });
-}
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,15 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ open: false, gzipSize: true, brotliSize: true }),
-    VitePWA({
-      registerType: "autoUpdate",
-      filename: "sw.js",
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
-      },
-      includeAssets: ["logo.png", "icon-192.png", "icon-512.png"],
-      manifest: {
-        name: "Clarity Tasks",
-        short_name: "C-Tasks",
-        display: "standalone",
-        background_color: "#000000",
-        theme_color: "#000000",
-        icons: [
-          { src: "icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "icon-512.png", sizes: "512x512", type: "image/png" },
-        ],
-      },
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
     }),
   ],
   publicDir: "public",
