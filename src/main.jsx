6ab10d05f27d2aae6ab41a7d386f0b8c7e7a3276
shennawy <ahmedshennawy997@gmail.com>
2025-12-31 11:12:53 +0200

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -10,5 +10,15 @@ createRoot(document.getElementById("root")).render(
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </StrictMode>
+  </StrictMode>,
 );
+
+// Register Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((reg) => console.log("Service Worker registered", reg))
+      .catch((err) => console.log("Service Worker registration failed:", err));
+  });
+}
