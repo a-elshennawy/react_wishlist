37c8130ded88ffbb0857524b94f64be70d4f3545
shennawy <ahmedshennawy997@gmail.com>
2025-12-31 12:13:24 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,37 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
+    visualizer({ open: true, gzipSize: true, brotliSize: true }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.png", "icon-192.png", "icon-512.png"],
+      manifest: {
+        name: "Clarity Tasks",
+        short_name: "C-Tasks",
+        description: "A user friendly task manager",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
     }),
   ],
   publicDir: "public",
