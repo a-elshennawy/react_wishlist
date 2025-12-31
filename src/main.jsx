dde6e3dae0d603e44b589c7a0273d7411ed700b8
shennawy <ahmedshennawy997@gmail.com>
2025-12-31 12:20:41 +0200

    chach

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import ThemeProvider from "./Components/Contexts/ThemeProvider.jsx";
 
-import { registerSW } from "virtual:pwa-register";
-
-registerSW({ immediate: true });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -6,30 +6,23 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ open: true, gzipSize: true, brotliSize: true }),
+    visualizer({ open: false, gzipSize: true, brotliSize: true }),
     VitePWA({
       registerType: "autoUpdate",
+      filename: "sw.js",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      },
       includeAssets: ["logo.png", "icon-192.png", "icon-512.png"],
       manifest: {
         name: "Clarity Tasks",
         short_name: "C-Tasks",
-        description: "A user friendly task manager",
-        theme_color: "#000000",
-        background_color: "#000000",
         display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
         icons: [
-          {
-            src: "icon-192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
+          { src: "icon-192.png", sizes: "192x192", type: "image/png" },
+          { src: "icon-512.png", sizes: "512x512", type: "image/png" },
         ],
       },
     }),
